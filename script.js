942e9f275b4cec237fe215d46527d8994f4987fe
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-08-02 13:07:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+function loadImage(image) {
+    const src = image.getAttribute('data-src');
+    if (!src) {
+        return;
+    }
+    image.src = src;
+}
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            loadImage(entry.target);
+            observer.unobserve(entry.target);
+        }
+    });
+}, options);
+
+const images = document.querySelectorAll('img.lazy-load');
+images.forEach(image => {
+    observer.observe(image);
+});
